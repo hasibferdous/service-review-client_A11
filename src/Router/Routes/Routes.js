4b6a80590e902441ec9adc6e66d85abd2cfe493b
hasibferdous <hasibferdous.ict@gmail.com>
2022-11-09 21:29:54 +0600

    header section completed

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter} from 'react-router-dom'
 import Main from '../../Layout/Main/Main';
 import Home from '../../Pages/Home/Home/Home';
+import Login from '../../Pages/Login/Login';
+import Register from '../../Pages/Register/Register';
 
     const router = createBrowserRouter([
         {
@@ -11,14 +13,14 @@ import Home from '../../Pages/Home/Home/Home';
                 path: '/',
                 element: <Home></Home>
             },
-            // {
-            //     path: '/login',
-            //     element: <Login></Login>
-            // },
-            // {
-            //     path: '/signup',
-            //     element: <SignUp></SignUp>
-            // },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            },
             // {
             //     path: '/checkout/:id',
             //     element: <Checkout></Checkout>,
